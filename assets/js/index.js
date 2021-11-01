28f3880c20851a7d58b75cac6da0a584e9322010
Виктор Нидерман <54781976+Zeref95@users.noreply.github.com>
2021-11-02 02:16:35 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,15 @@
-const pageList = {
-  'about': {
-    title: {
-      left: {
-        h1: 'Project concept'
-      },
-      right: {
-        p: 'The prestigious location, author\'s architecture, well-maintained closed territory and its own branched infrastructure create a unique atmosphere and the most comfortable conditions to live, create&nbsp;ad&nbsp;love.'
-      }
-    },
-    showBlock: '#about'
-  },
-  'gallery': {
-    title: {
-      left: {
-        h1: 'Gallery',
-        p: 'Gallery is divided into sections according to their residences'
-      },
-      right: {
-        p: ''
-      }
-    },
-    showBlock: '#gallery'
-  },
-};
-let currentPage = 'gallery';
+const pageList = ['gallery', 'about'];
+let currentPage = 'about';
 
 function goToPage(pageName) {
-  if (Object.prototype.hasOwnProperty.call(pageList, pageName)
+  if (pageList.includes(pageName)
   && pageName !== currentPage) {
-    document.querySelector(pageList[currentPage].showBlock).style.display = 'none';
+    document.querySelectorAll('.page-' + currentPage).forEach(el => {
+      el.classList.add('d-none')
+    });
     currentPage = pageName;
-    updatePage(currentPage);
+    document.querySelectorAll('.page-' + currentPage).forEach(el => {
+      el.classList.remove('d-none')
+    });
   }
-}
-
-function updatePage(pageName) {
-  document.querySelector('#main-title div:first-child h1').textContent
-    = pageList[pageName].title.left.h1 ?? '';
-  document.querySelector('#main-title div:first-child p').textContent
-    = pageList[pageName].title.left.p ?? '';
-  document.querySelector('#main-title div:last-child p').innerHTML
-    = pageList[pageName].title.right.p ?? '';
-
-  document.querySelector(pageList[pageName].showBlock).style.display = 'flex';
-
 }
